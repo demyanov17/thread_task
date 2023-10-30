e47fe54f619a9a9b9b0a6539a38822a89a1a2977
demyanov17 <98758895+demyanov17@users.noreply.github.com>
2023-10-30 16:01:13 +0300

    Add files via upload

--- print.cpp
+++ print.cpp
@@ -2,13 +2,17 @@
 #include <unistd.h>
 
  
-void print_(){
+void print_()
+{
+
 	int k=1;
-	for(;;){
-	    for (int i=0; i<k; ++i){
+
+	for(;;)
+	{
+	    for (int i=0; i<k; ++i)
 	        std::cout <<'*';
-	    }
 	    k++;
 	    std::cout << std::endl;
 	    sleep(1);
-}}
+	}
+}
